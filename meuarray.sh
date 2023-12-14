bd903e29c6536fbcbd7c13439e86248ea55f44c7
flavionizette <flavio.doni17@gmail.com>
2023-12-13 23:30:25 -0300

    New files

---
+++ meuarray.sh
@@ -0,0 +1,11 @@
+meuarray=(1 2 3 4 5 6 7)
+
+echo ${meuarray[*]}
+
+unset meuarray[6]
+
+meuarray[6]='oito'
+
+echo ${meuarau[6]}
+exit
+
---
+++ meuscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Este é meu primeiro script do bash
+# Linhas com jogo da velha são ignoradas
+
+cd/
+ls -l
+echo "Esta foi a listagem de arquivos e diretórios dentro da raiz"
+echo -n "O usuario logado no momento é: "
+whoami
